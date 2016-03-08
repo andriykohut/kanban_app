a853423e99b81e7a5cf3504a085b6298d89b5136
Andriy Kogut <kogut.andriy@gmail.com>
2016-03-08 13:52:38 +0200

    initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const NpmInstallWebpackPlugin = require('npm-install-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+switch (TARGET) {
+  case 'start':
+    module.exports = merge(common, {
+      devtool: 'eval-source-map',
+      devServer: {
+        contentBase: PATHS.build,
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+        stats: 'errors-only',
+        host: process.env.HOST,
+        port: process.env.PORT
+      },
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new NpmInstallWebpackPlugin({ save: true })
+      ]
+    });
+    break;
+  case 'build':
+    module.exports = merge(common, {});
+    break;
+  default:
+    module.exports = common;
+}
